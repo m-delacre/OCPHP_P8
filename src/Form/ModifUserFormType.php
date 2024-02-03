bb14235ae7603d5792d6b593eae0e47c3b5b5e4d
mdelacre <mdelacre@protonmail.com>
2024-02-04 00:25:24 +0100

    modification du style des formulaires

--- src/Form/ModifUserFormType.php
+++ src/Form/ModifUserFormType.php
@@ -4,10 +4,11 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class ModifUserFormType extends AbstractType
@@ -15,11 +16,16 @@ class ModifUserFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('email')
+            ->add('username', TextType::class, [
+                'label'=>"Nom d'utilisateur :"
+            ])
+            ->add('email', TextType::class, [
+                'label'=>"Adresse mail :"
+            ])
             ->add('password', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label'=>"Mot de passe :",
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'required' => false,
@@ -33,7 +39,7 @@ class ModifUserFormType extends AbstractType
                 ],
             ])
             ->add('isAdmin', CheckboxType::class, [
-                'label'=>'is admin ?',
+                'label'=>"L'utilisateur est admin ?",
                 'required'=>false,
                 'mapped' => false,
                 'data' => $options['is_admin']
